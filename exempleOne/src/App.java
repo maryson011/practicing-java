4143247727a7fb9fab81e0b204403a82d4874321
maryson gleison <maryson011@gmail.com>
2024-09-09 11:26:22 -0300

    feat: threads (main not blocked)

--- exempleOne/src/App.java
+++ exempleOne/src/App.java
@@ -1,7 +1,20 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-        CompletableFutureExemplo completableFutureExemplo = new CompletableFutureExemplo();
-        completableFutureExemplo.run();
+        MultiThreadExample multiThreadExample1 = new MultiThreadExample("thread 1");
+        MultiThreadExample multiThreadExample2 = new MultiThreadExample("thread 2");
+        MultiThreadExample multiThreadExample3 = new MultiThreadExample("thread 3");
+
+        Thread threa1 = new Thread(multiThreadExample1);
+        Thread threa2 = new Thread(multiThreadExample2);
+        Thread threa3 = new Thread(multiThreadExample3);
+
+        threa1.start();
+        threa2.start();
+        threa3.start();
+
+        for (int i =0; i < 10; i++) {
+            System.out.println("Main thread valor atual do lood "+i);
+        }
     }
 }
