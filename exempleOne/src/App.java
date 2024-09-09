61981da8b0cc4997439d8fb61908aface4afceb3
maryson gleison <maryson011@gmail.com>
2024-09-09 10:26:04 -0300

    feat: completableFuture supplyAssync #1

--- exempleOne/src/App.java
+++ exempleOne/src/App.java
@@ -1,24 +1,7 @@
-import java.util.concurrent.CompletableFuture;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
         CompletableFutureExemplo completableFutureExemplo = new CompletableFutureExemplo();
-
-        CompletableFuture completableFuture = CompletableFuture.runAsync(() -> {
-            PokemonAbilityFetch pokemonAbilityFetch = new PokemonAbilityFetch("pikachu");
-            pokemonAbilityFetch.fetch();
-        });
-
-        for(int i = 0; i < 10; i++) {
-            System.out.println(i);
-            Thread.sleep(3000);
-        }
-        try {
-            completableFuture.get();
-            System.out.println("Main thread is running");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        completableFutureExemplo.run();
     }
 }
--- exempleOne/src/CompletableFutureExemplo.java
+++ exempleOne/src/CompletableFutureExemplo.java
@@ -3,10 +3,13 @@ import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureExemplo {
     public void run() throws ExecutionException, InterruptedException {
-        CompletableFuture.runAsync(() -> {
+        CompletableFuture.supplyAsync(() -> {
             PokemonAbilityFetch pokemonAbilityFetch = new PokemonAbilityFetch("pikachu");
                 pokemonAbilityFetch.fetch();
-        });
+                return "done";
+        }).thenAccept((result) -> {
+            System.out.println("task result: " + result);
+        }).get();
     }
 }
 
